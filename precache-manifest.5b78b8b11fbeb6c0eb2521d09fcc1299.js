f316bffe46e098628b47bf0647b475de29500e50
JaneySlinger <JaneySlinger@users.noreply.github.com>
2021-10-03 19:02:51 +0000

    deploy: ad2b4296befb4c0c80069bb9f34a74ecc8891638

---
+++ precache-manifest.5b78b8b11fbeb6c0eb2521d09fcc1299.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b26628511e1342048977aebaa7418d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d11e06ef1998f81d532",
+    "url": "/static/css/main.a5e67f46.chunk.css"
+  },
+  {
+    "revision": "f4371c598301d2f70f74",
+    "url": "/static/js/2.e830d736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e830d736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d11e06ef1998f81d532",
+    "url": "/static/js/main.22b74c68.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "7a34cf4279c6f8f8c97a1250ccce40a4",
+    "url": "/static/media/logo.7a34cf42.png"
+  }
+]);
